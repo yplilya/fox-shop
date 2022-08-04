3b5abff3a736e741ab8d388bed1ae2911e50faed
yplilya <ypl2002ilya@gmail.com>
2022-08-04 12:46:22 +0200

    Added frontend components, connected sanity

--- pages/_app.js
+++ pages/_app.js
@@ -2,12 +2,14 @@ import React from "react";
 import '../styles/globals.css'
 import { Layout } from "../components";
 import { StateContext } from "../context/StateContext";
+import { Toaster } from "react-hot-toast";
 
 function MyApp({ Component, pageProps }) {
   return (
       <StateContext>
           <Layout>
-                <Component {...pageProps} />
+              <Toaster />
+              <Component {...pageProps} />
           </Layout>
       </StateContext>
   )
